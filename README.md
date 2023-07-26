ddb420ca03d92fc5b23d88bed09de67b176b71a5
Jonas Schäfer <jonas.schaefer@cloudandheat.com>
2023-07-26 08:29:16 +0200

    Prettify!

--- README.md
+++ README.md
@@ -1,12 +1,23 @@
-# rabbitmq_head_message_timestamp_exporter
+# rabbitmq\_head\_message\_timestamp\_exporter
 
-```
-mqmon.py -h
+## Overview
+
+This is a small proof-of-concept tool to obtain the timestamps of the
+last OpenStack notification messages, by type, sent to Ceilometer.
+
+The idea is to be able to write prometheus alerts if the Ceilometer
+queues are suspiciously quiet, in particular when polling events are
+not coming in as expected.
+
+## Usage
+
+See the corresponding help outputs:
+
+```console
+$ mqmon.py -h
 ```
 
-or
-```
-mqmon_exporter.py -h
+```console
+$ mqmon_exporter.py -h
 ```
 
-to get help
--- rabbitmq_lib.py
+++ rabbitmq_lib.py
@@ -5,6 +5,7 @@ import pika
 import json
 import ast
 
+
 def get_broker_connection(host, user, password):
     credentials = pika.PlainCredentials(user, password)
     parameter = pika.ConnectionParameters(
@@ -14,42 +15,46 @@ def get_broker_connection(host, user, password):
     connection = pika.BlockingConnection(parameter)
     return connection
 
+
 def get_channel(connection):
     channel = connection.channel()
     return channel
+
 
 def get_messages(host, user, password, queue, on_message_callback=None):
     connection = get_broker_connection(host, user, password)
     channel = get_channel(connection)
 
     if not on_message_callback:
+
         def on_message_callback(ch, method, properties, body):
             print(ch)
             print(method)
             print(properties)
             print(" [x] Received %r" % body)
 
-    channel.basic_consume(queue=queue,
-                          on_message_callback=on_message_callback,
-                          auto_ack=False
-                          )
+    channel.basic_consume(
+        queue=queue, on_message_callback=on_message_callback, auto_ack=False
+    )
 
-    print(' [*] Waiting for messages. To exit press CTRL+C')
+    print(" [*] Waiting for messages. To exit press CTRL+C")
     channel.start_consuming()
+
 
 def get_oslo_message(message):
     message_dict = json.loads(message)
     print(message_dict)
-    oslo_message = ast.literal_eval(message_dict['oslo.message'])
+    oslo_message = ast.literal_eval(message_dict["oslo.message"])
     return oslo_message
 
 
 def get_message_timestamp(message):
     oslo_message = get_oslo_message(message)
-    timestamp = oslo_message['timestamp']
+    timestamp = oslo_message["timestamp"]
     return timestamp
+
 
 def get_message_payload(message):
     oslo_message = get_oslo_message(message)
-    payload = oslo_message['payload']
+    payload = oslo_message["payload"]
     return payload
